27adfee9722439992e6fcf22c8e52f4fadb810cb
rcortezk9 <rcortezk9@gmail.com>
2017-06-08 15:16:03 -0500

    redirect users that login correctly have access to profile otherwise get redirected to login page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -17,6 +17,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("username", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
