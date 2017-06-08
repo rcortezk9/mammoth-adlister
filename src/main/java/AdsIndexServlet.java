a2d460b223a7f849019d793d7c6bc13c9bdfd2dd
rcortezk9 <rcortezk9@gmail.com>
2017-06-08 10:30:45 -0500

    created ads index servlet for the all method on ads dao class to get all listings

---
+++ src/main/java/AdsIndexServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by renecortez on 6/8/17.
+ */
+
+@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+
+    }
+}
