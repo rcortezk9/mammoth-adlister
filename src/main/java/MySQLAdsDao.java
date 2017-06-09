d716b06f5dc0f2e702641339ceeac796ad67c3d2
rcortezk9 <rcortezk9@gmail.com>
2017-06-09 14:57:19 -0500

    Created a class named MySQLAdsDao that implements the Ads interface
    implement the methods in the Ads interface
    
    created methods to retrieve ads from the database and insert new ads into the database

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,3 +1,6 @@
+import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,7 +10,44 @@ import java.util.List;
 public class MySQLAdsDao implements Ads {
 
     @Override
-    public List<Ad> all() {
+    public List<Ad> all() throws SQLException {
+
+        Config config = new Config();
+
+        Connection connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUsername(),
+                config.getPassword()
+        );
+
+        //insert(connection);
+
+        String query = "SELECT * FROM ads";
+
+        System.out.println("Selecting everything from the ads table");
+
+        Statement stmt = connection.createStatement();
+
+        ResultSet rs = stmt.executeQuery(query);
+
+        List<Ad> ads = new ArrayList<>();
+
+        while (rs.next()) {
+
+            long user_id = rs.getLong("user_id");
+
+            String title = rs.getString("title");
+
+            String description = rs.getString("description");
+
+            Ad a = new Ad(user_id, title, description);
+
+            ads.add(a);
+
+            System.out.println(a.getTitle());
+
+        }
+
         return null;
     }
 
@@ -15,4 +55,23 @@ public class MySQLAdsDao implements Ads {
     public Long insert(Ad ad) {
         return null;
     }
+
+        public static void insert(Connection connection, HttpServletRequest request)throws SQLException{
+
+            Statement statement = connection.createStatement();
+
+            String user_id = request.getParameter("user_id");
+
+            String title = request.getParameter("title");
+
+            String description = request.getParameter("description");
+
+            String query = "INSERT INTO ads(user_id, title, description) VALUES";
+
+            query += "(" + user_id + ", " + title + ", " + description + ")";
+
+            statement.execute(query);
+
+        }
+
 }
